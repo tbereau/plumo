fbbd152eb2dc87937bba1b4f92780774f9304ed1
Tristan Bereau <bereau@mpip-mainz.mpg.de>
2015-11-11 10:18:54 +0100

    Bugfix numerical derivative for TI

--- cgtools/analysis/ti.tcl
+++ cgtools/analysis/ti.tcl
@@ -23,19 +23,29 @@ proc ::cgtools::analysis::ti::printav_ti { } {
   # Do nothing
 }
 
-proc ::cgtools::analysis::ti::setup_ti { {eqtime 0} {delta ""} } {
+proc ::cgtools::analysis::ti::setup_ti {  dlambda {eqtime 0} {delta ""} } {
   global ::cgtools::analysis::outputdir
   global ::cgtools::analysis::iotype
   global ::cgtools::analysis::suffix
   variable file_ti
   variable tiequitime
   variable lmbd
+  variable dlmbd $dlambda
   variable ::cgtools::lambda_coupling
   variable ::cgtools::softcore_flag
   variable ::cgtools::softcore_delta
 
   set lmbd $lambda_coupling
   set tiequitime $eqtime
+
+  # Check that dlambda is strictly positive
+  if {[expr $dlmbd <= 0.0]} {
+    ::mmsg::err [namespace current] "dlambda must be strictly positive"
+  }
+  # Check that lambda +/- dlambda is within [0,1]
+  if { [expr $lmbd - $dlmbd < 0.0] || [expr $lmbd + $dlmbd > 1.0]} {
+    ::mmsg::err [namespace current] "lambda +/- dlambda not within [0,1]"
+  }
 
   set file_ti "$outputdir/time_vs_ti_$lmbd"
   if { [file exists $file_ti] } {
@@ -45,7 +55,7 @@ proc ::cgtools::analysis::ti::setup_ti { {eqtime 0} {delta ""} } {
   }
   set pipe [open $file_ti $iotype]
   if { $newfile || $iotype == "w"} {
-    puts $pipe "\# time dE/dlambda E"
+    puts $pipe "\# time dE/dlambda"
   }
   close $pipe
 
@@ -66,15 +76,15 @@ proc ::cgtools::analysis::ti::analyze_ti { } {
   variable file_ti
   variable tiequitime
   variable lmbd
+  variable dlmbd
   variable ::cgtools::systemtemp
   variable ::cgtools::lambda_coupling
 
   set currenttime [setmd time]
   if { $currenttime >= $tiequitime } {
-    # Now change of force field with original Hamiltonian (lambda = 1)
-    ::cgtools::forcefield::update_peptide_ff 1.0
-
-    set epot 0.0
+    # Evaluate force field with lambda+dlambda
+    ::cgtools::forcefield::update_peptide_ff [expr $lmbd + 0.5*$dlmbd]
+    set epotp 0.0
     # Sum up only peptide-lipid contributions
     # lipid beads
     for {set lb 0} {$lb < 8} {incr lb} {
@@ -83,27 +93,29 @@ proc ::cgtools::analysis::ti::analyze_ti { } {
         if { [catch {set epair [analyze energy nonbonded $lb $pb]}] } {
           set epair 0.0
         }
-        set epot [expr "$epot + $epair"]
+        set epotp [expr "$epotp + $epair"]
       }
     }
+    # Evaluate force field with lambda-dlambda
+    ::cgtools::forcefield::update_peptide_ff [expr $lmbd - 0.5*$dlmbd]
+    set epotm 0.0
+    # Sum up only peptide-lipid contributions
+    # lipid beads
+    for {set lb 0} {$lb < 8} {incr lb} {
+      # peptide beads
+      for {set pb 8} {$pb < 43} {incr pb} {
+        if { [catch {set epair [analyze energy nonbonded $lb $pb]}] } {
+          set epair 0.0
+        }
+        set epotm [expr "$epotm + $epair"]
+      }
+    }
+
     # Now revert back to original force field with lambda_i
     ::cgtools::forcefield::update_peptide_ff $lmbd
 
-    set epoto 0.0
-    # Sum up only peptide-lipid contributions
-    # lipid beads
-    for {set lb 0} {$lb < 8} {incr lb} {
-      # peptide beads
-      for {set pb 8} {$pb < 43} {incr pb} {
-        if { [catch {set epair [analyze energy nonbonded $lb $pb]}] } {
-          set epair 0.0
-        }
-        set epoto [expr "$epoto + $epair"]
-      }
-    }
-
     set pipe [open $file_ti a]
-    puts $pipe [format "%16.4f %9.4f %9.4f" $currenttime $epot $epoto]
+    puts $pipe [format "%16.4f %9.4f" $currenttime [expr ($epotp-$epotm)/$dlmbd]]
     close $pipe
   }
 }
